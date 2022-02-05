14a9938d8feff492cf11e49ac81e06ae76d89440
Max Lutsenko <makc199710@me.com>
2022-02-05 21:14:43 +0200

    Add routing HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from "react-router-dom";
+
+const HatPage = () => {
+  return(
+  <div>
+    <h1>hello</h1>
+  </div>
+  )}
 
 function App() {
   
-  return <div><HomePage/></div>;
-}
+  return( 
+      <div>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='shop/hats' element={<HatPage/>}/>
+        </Routes>
+      </div>
+);}
 
 export default App;
